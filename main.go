9d87decde81ab8c339a86b5287f05ab3781330bc
Hemant041104 <142302155+Hemant041104@users.noreply.github.com>
2025-07-12 21:34:01 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,27 +6,23 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
 	http.ServeFile(w, r, "static/courses.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
 	http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
-
+	http.HandleFunc("/", homePage)       // ✅ This handles root `/`
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
